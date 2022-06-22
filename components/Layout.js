f8e69d8539745736b82d530db30ec56e194e3817
aleksejem <alexeyhong10@gmail.com>
2022-06-22 18:06:42 +0500

    Added layout.

---
+++ components/Layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default (props) => {
+    return (<div>
+        <h1>2323</h1>
+        {props.children}
+        <h1>23232323</h1>
+    </div>)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import factory from "../ethereum/factory";
+import {Card, Button} from 'semantic-ui-react'
+import Layout from "../components/Layout";
 
 class CampaignIndex extends Component {
     static async getInitialProps() {
@@ -7,8 +9,37 @@ class CampaignIndex extends Component {
         return {campaigns}
     }
 
+    renderCampaigns() {
+        const items = this.props.campaigns.map((address) => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true,
+            }
+        })
+
+        return <Card.Group items={items}/>
+    }
+
     render() {
-        return <div>{this.props.campaigns}</div>
+        return (
+            <Layout>
+                <div>
+                    <link
+                        async
+                        rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+                    />
+                    <h3>Open Campaigns</h3>
+                    {this.renderCampaigns()}
+                    <Button
+                        content="Create Campaign"
+                        icon="add circle"
+                        primary
+                    />
+                </div>
+            </Layout>
+        )
     }
 }
 
