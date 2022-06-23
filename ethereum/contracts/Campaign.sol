99d9ec6bb25759182a8553f262e6f6520aeeefc9
aleksejem <alexeyhong10@gmail.com>
2022-06-23 19:24:45 +0500

    Recompiled contracts

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -25,7 +25,7 @@ contract Campaign {
 
     Request[] public requests;
     address public manager;
-    uint public minimumContribution;
+    uint publReic minimumContribution;
     mapping(address => bool) public approvers;
     uint public approversCount;
 
@@ -48,11 +48,11 @@ contract Campaign {
 
     function createRequest(string description, uint value, address recipient) public restricted {
         Request memory newRequest = Request({
-           description: description,
-           value: value,
-           recipient: recipient,
-           complete: false,
-           approvalCount: 0
+        description : description,
+        value : value,
+        recipient : recipient,
+        complete : false,
+        approvalCount : 0
         });
 
         requests.push(newRequest);
@@ -76,5 +76,20 @@ contract Campaign {
 
         request.recipient.transfer(request.value);
         request.complete = true;
+    }
+    function getSummary() public view returns (
+        uint, uint, uint, uint, address
+    ) {
+        return (
+        minimumContribution,
+        this.balance,
+        requests.length,
+        approversCount,
+        manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
     }
 }
